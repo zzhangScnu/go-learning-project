0295e2dfd4f64cd9111b555fed217e92009fda36
zhangzhang <zhanglihua_scnu@yeah.net>
2022-04-11 15:09:33 +0800

    feat: pointer and value

--- interface/animal_test.go
+++ interface/animal_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/go-learning-project/interface/cat"
 	"testing"
 )
@@ -11,4 +12,31 @@ func TestAnimal(t *testing.T) {
 	if oneCat.Color() != wantedColor {
 		t.Errorf("test failed. result is: %s", oneCat.Color())
 	}
+}
+
+func TestSetter(t *testing.T) {
+	catAddr := cat.NewCatAddr("black", 10, 10)
+	catAddr.SetColor("white")
+	fmt.Println(catAddr)
+	// 指针会隐式转换为值，去调用set方法。但是只是设置到了拷贝的入参里
+	catAddr.SetColorForValue("yellow")
+	fmt.Println(catAddr)
+}
+
+func TestSetterForValue(t *testing.T) {
+	catVal := cat.NewCat("black", 10, 10)
+	// 值会隐式转换为指针，去调用set方法。由于方法接收器是指针，设置到了对应的内存地址上
+	catVal.SetColor("white")
+	fmt.Println(catVal)
+	catVal.SetColorForValue("yellow")
+	fmt.Println(catVal)
+}
+
+func TestPointer(t *testing.T) {
+	catAddr := cat.NewCatAddr("black", 10, 10)
+	fmt.Println(catAddr)  // &值
+	fmt.Println(&catAddr) // 内存地址
+	fmt.Println(*catAddr) // 值
+	num := 5
+	fmt.Println(&num) // 内存地址
 }
--- interface/cat/Cat.go
+++ interface/cat/Cat.go
@@ -7,8 +7,14 @@ type Cat struct {
 	name   string
 }
 
+// NewCat 可以在返回指针的构造函数之上取值，因为指针指向的是分配的真实地址
+// 但是不能在返回值的构造函数之上取地址，因为返回的值已经是拷贝的，而不是实例化的原值了
 func NewCat(color string, height float64, weight float64) Cat {
-	return Cat{
+	return *NewCatAddr(color, height, weight)
+}
+
+func NewCatAddr(color string, height float64, weight float64) *Cat {
+	return &Cat{
 		color:  color,
 		height: height,
 		weight: weight,
@@ -35,4 +41,12 @@ func (c Cat) Name() string {
 // SetName 方法的接收器参数是指针，这样才能真实地改变实参
 func (c *Cat) SetName(name string) {
 	c.name = name
+}
+
+func (c *Cat) SetColor(color string) {
+	c.color = color
+}
+
+func (c Cat) SetColorForValue(color string) {
+	c.color = color
 }
